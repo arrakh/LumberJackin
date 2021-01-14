76050b492529a2037e1ade3bd6147562e997558a
Ilham Jalu Prakosa <ilham.jalu00@gmail.com>
2021-01-15 06:43:17 +0700

    Hosting Login

--- Assets/LoginSystem/DatabaseLogin.cs
+++ Assets/LoginSystem/DatabaseLogin.cs
@@ -44,11 +44,13 @@ public class DatabaseLogin : MonoBehaviour
         form.AddField("password", pass.text);
         form.AddField("email", email.text);
 
-        WWW www = new WWW("http://localhost/LumberJackin/Register.php", form);
+        WWW www = new WWW("https://lumberjackin.000webhostapp.com/LumberJackin/Register.php", form);
+        Debug.Log(username.text);
         yield return www;
+        Debug.Log(www.text);
 
         SceneManager.LoadScene("Login");
-        Debug.Log(www.text);
+        
     }
 
     public void CallLogin()
@@ -69,7 +71,7 @@ public class DatabaseLogin : MonoBehaviour
         form.AddField("username", username.text);
         form.AddField("password", pass.text);
 
-        WWW www = new WWW("http://localhost/LumberJackin/Login.php", form);
+        WWW www = new WWW("https://lumberjackin.000webhostapp.com/LumberJackin/Login.php", form);
         yield return www;
 
         if (www.isDone)
@@ -105,7 +107,7 @@ public class DatabaseLogin : MonoBehaviour
         WWWForm form = new WWWForm();
         form.AddField("username", username.text);
 
-        WWW www = new WWW("http://localhost/LumberJackin/ForgotPass.php", form);
+        WWW www = new WWW("https://lumberjackin.000webhostapp.com/LumberJackin/ForgotPass.php", form);
         yield return www;
 
         if (www.isDone)
